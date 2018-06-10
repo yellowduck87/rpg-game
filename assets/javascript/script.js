f0b518d2f7eafd71b0ddef702795a3e2ab5b67be
Megan Evans <megan.evans.ducky@gmail.com>
2018-06-09 22:41:10 -0600

    small bug fix

---
+++ assets/javascript/script.js
@@ -0,0 +1,31 @@
+//1 create caracters as objects
+//a player 1 
+//hp
+//ap
+//cap
+//b player 1 
+//hp
+//ap
+//cap 
+//c player 1 
+//hp
+//ap
+//cap 
+//d player 1 
+//hp
+//ap
+//cap   
+
+//1.5 when a character attacks, its ap goes up incrementaly.
+
+//2 when one chrater is selected, the rest are moved to enemies possition
+
+//3 when one enemy is selected, it is moved to the defender possition
+
+//4 when a character attacks, it removes hp from the defender, and the defender responds by removing hp from the chracter
+
+//5 when any character gets to 0 or less hp, the charcter is defeated
+
+//6 when a defnding character is defeted, the image goes away
+
+//7 if the selected character defeats the other three, the user wins.
