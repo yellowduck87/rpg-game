6614ed474d61a489804405c62a5e35eaaa649df5
Megan Evans <megan.evans.ducky@gmail.com>
2018-06-13 00:00:06 -0600

    added working js, have avatar battles coded---now need div placement

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,69 +1,114 @@
-  
+  //1.5 when a character attacks, its ap goes up incrementaly.
 
-//1.5 when a character attacks, its ap goes up incrementaly.
+  //2 when one chrater is selected, the rest are moved to enemies possition
 
-//2 when one chrater is selected, the rest are moved to enemies possition
+  //3 when one enemy is selected, it is moved to the defender possition
 
-//3 when one enemy is selected, it is moved to the defender possition
+  //4 when a character attacks, it removes hp from the defender, and the defender responds by removing hp from the chracter
 
-//4 when a character attacks, it removes hp from the defender, and the defender responds by removing hp from the chracter
+  //5 when any character gets to 0 or less hp, the charcter is defeated
 
-//5 when any character gets to 0 or less hp, the charcter is defeated
+  //6 when a defnding character is defeted, the image goes away
 
-//6 when a defnding character is defeted, the image goes away
-
-//7 if the selected character defeats the other three, the user wins.
-$(document).ready(function () {
-    var counter = 0;
-    var ememiesDefeated = 0;
-    var charImg = "";
-    var char = [];
-
-    var characters = {
-        char1: {
-            hp: 120,
-            ap: 25,
-            dAp: 25,
-            image: 'assets/images/quack1.jpg',
-        },
-        char2: {
-            hp: 180,
-            ap: 18,
-            dAp: 18,
-            image: 'assets/images/quack1.jpg',
-        },
-        char3: {
-            hp: 200,
-            ap: 10,
-            dAp: 10,
-            image: 'assets/images/quack1.jpg',
-        },
-        char4: {
-            hp: 160,
-            ap: 20,
-            dAp: 20,
-            image: 'assets/images/quack1.jpg',
-        }
-    }
-
-    char = ["#char1", "#char2", "#char3", "#char4"];
-    console.log(char[1])
+  //7 if the selected character defeats the other three, the user wins.
 
 
-    // for (var i = 0; i < char.length; i++) {
-    //     var charImg = $("img>");
-    //     var src = characters.char2.image;
-    //     charImg.addClass("toon-img");
-    //     charImg.attr("src", src);
-    //     $("#readyP1").append(charImg);
-    // }
+  // computed member acces
+
+  var characters = {
+      char1: {
+          hp: 120,
+          ap: 25,
+          mult: 10,
+          dAp: 25,
+      },
+      char2: {
+          hp: 180,
+          ap: 18,
+          mult: 9,
+          dAp: 18,
+      },
+      char3: {
+          hp: 200,
+          ap: 10,
+          mult: 7,
+          dAp: 10,
+      },
+      char4: {
+          hp: 160,
+          ap: 20,
+          mult: 8,
+          dAp: 20,
+      },
+  }
+
+  $(document).ready(function () {
+              var counter = 0;
+              var ememiesDefeated = 0;
+              var char = [];
+              var combatChar;
+              var combatChosen = false;
+              var enemyChosen = false;
+              var avatar;
+              var enemy;
+
+              char = Object.keys(characters);
+              var imageSrc = ["http://via.placeholder.com/100x100", "http://via.placeholder.com/100x100", "http://via.placeholder.com/100x100", "http://via.placeholder.com/100x100"];
+
+              loadNewGame = function () {
+                  for (var i = 0; i < imageSrc.length; i++) {
+                      var charImg = $("<img>");
+                      source = imageSrc[i];
+                      charImg.attr("id", char[i]);
+                      charImg.addClass("toon-img").attr("src", source);
+                      $("#readyP1").append(charImg);
+                  }
+              }
+
+              console.log(avatar);
+              console.log(combatChosen);
+
+              //   gameCode = function () {
+              toonPicker = function () {
+                  $(".toon-img").on("click", function (event) {
+                      if (enemyChosen) {
+                          return false;
+                      }
+
+                      if (combatChosen) {
+                          enemy = event.target.id;
+                          enemyChosen = true;
+                          console.log("enemy = " + enemy);
+                          console.log(characters[enemy]);
+
+                      } else {
+                          avatar = event.target.id;
+                          combatChosen = true;
+                          console.log("avatar = " + avatar);
+                          console.log(characters[avatar]);
+                      }
+                  });
+              }
+              attack = function () {
+                  $("#attack").on("click", function () {
+                          if (enemyChosen && characters[enemy].hp > 0) {
+                              characters[enemy].hp -= characters[avatar].ap;
+                              characters[avatar].ap += characters[avatar].mult;
+                              if (characters[enemy].hp > 0) {
+                                  characters[avatar].hp -= characters[enemy].dAp;
+                              } else {
+                                  alert("you have deafeted " + enemy);
+                                  enemyChosen = false;
+                                  toonPicker();
+                              }
+                              console.log(characters[avatar])
+                            }
+                          })
+                  }
+
+                  loadNewGame();
+                  toonPicker();
+                  attack();
 
 
-    loadNewGame = function () {
-    
-
-    }
-    // loadNewGame();
-
-
-});
+              });
